13c1fc4247128430cc11d00660db421e1c92b002
sumisa91 <sumisa91@ubuntu>
2013-11-07 14:58:43 +1000

    Refactoring

--- MyFilter.php
+++ MyFilter.php
@@ -2,27 +2,27 @@
 
 require_once('lib/Phirehose.php');
 require_once('lib/OauthPhirehose.php');
-
 require_once ('AWSinsertRawTweets.php');
 
 
 /**
- * class using Phirehose to display a live filtered stream using track words 
+ * Class using Phirehose to display a live stream 
+ * of tweets from twitter 
  */
 class FilterTrackConsumer extends OauthPhirehose {
 
     /**
      * Enqueue each status
-     * @param string $status
+     * Function implemented from OauthPhirehose
      */  
     
     public function enqueueStatus($status) {     
         
-        //print  $status."\n\n";
+        //Store tweets from the stream into 
+        //MSrawTweets of DynamoDB
         storeRawTweetsInDatabase($status);
-
     }
-}
+}//end of class
 
 
 // The OAuth credentials
@@ -31,27 +31,16 @@ define("TWITTER_CONSUMER_SECRET", "NR6GEcj09DCBn1K2nNtfYM4wp7bBHXnRkD2ekuI");
 define("OAUTH_TOKEN", "543781426-Q2Y6IugeGaoA9tGh5PrpkPH00MFtQ2PbqsiVHRN6");
 define("OAUTH_SECRET", "hVNkoq7IbpzmD3wi57gF2tUU8bbgZ3Kv9wKJ2JPk2o");
 
-//gets the trackwords from the command live ie the exec command in Start.php
-//$trackWords = unserialize($argv[1]);
-//$boundingBox = unserialize($argv[2]);
-
-//$trackWords = array('the'); //testing only ie no-one more popular than Justin is there?
-//$boundingBox = "-28.03728,152.452799,-26.7775,153.55292";
-
 // Create the stream object from above class and start streaming
 $sc = new FilterTrackConsumer(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_SAMPLE);
+//Set tweet text language to English
 $sc->setLang('en');
-//$sc->setTrack($trackWords);
-//$sc->setLocations(array(array($boundingBox)));
 
+//Delete tweets from MSrawTweets table
+//from the past hour
 $cmd = "php AWSdeleteRawTweets.php";
 $pid = exec("nohup $cmd > /dev/null 2>&1 & echo $!");
 
-
+//Begin receiving tweets from the twitter stream
 $sc->consume();
-
-
-
-
-
 ?>
